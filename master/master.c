4ed3a8686ee41fe943301df60f8ead78143a470a
shaanzie <ishaanlagwankar@gmail.com>
2019-10-30 15:12:06 +0530

    FIXED

--- master/master.c
+++ master/master.c
@@ -4,6 +4,7 @@
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
+#include <sys/types.h>
 #include <netinet/in.h> 
 #include <string.h> 
 #define PORT 8080 
@@ -56,7 +57,7 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE); 
         } 
         valread = read( new_socket , buffer, 1024); 
-        execvp(buffer, "."); 
+        system(buffer); 
         send(new_socket , hello , strlen(hello) , 0 ); 
         printf("Hello message sent\n"); 
         close(new_socket);
